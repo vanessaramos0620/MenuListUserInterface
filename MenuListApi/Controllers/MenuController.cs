6e5e78d37605cd94bc8cb95f92c29b0b33f7b215
Mitzi Villanueva <Mitzi Villanueva@LAPTOP-2V85TBH6>
2024-07-19 23:30:00 +0800

    Add project files.

---
+++ MenuListApi/Controllers/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MenuListBusinessLogic;
+using MenuListModel;
+using System.Collections.Generic;
+
+namespace MenuListApi.Controllers
+{
+    [ApiController]
+    [Route("api/menu")]
+    public class MenuController : ControllerBase
+    {
+        private readonly MenuService _menuService;
+
+        public MenuController(MenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Menu> GetAllMenus()
+        {
+            return _menuService.GetAllMenus();
+        }
+
+        [HttpGet("{order}")]
+        public ActionResult<Menu> GetMenu(string order)
+        {
+            var menu = _menuService.GetMenu(order);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return menu;
+        }
+
+        [HttpPost]
+        public IActionResult PostMenu(Menu menu)
+        {
+         
+            return Ok();
+        }
+
+        [HttpPatch("{order}")]
+        public IActionResult UpdateMenu(string order, Menu updatedMenu)
+        {
+          
+            return Ok();
+        }
+
+        [HttpDelete("{order}")]
+        public IActionResult DeleteMenu(string order)
+        {
+         
+            return Ok();
+        }
+    }
+}
---
+++ MenuListDataLayer/MenuDataService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using MenuListModel;
+
+namespace MenuListDataLayer
+{
+    public class MenuDataService
+    {
+        static string connectionString
+            //= "Data Source=LAPTOP-2V85TBH6\\SQLEXPRESS01;Initial Catalog=VanessaRestaurant;Integrated Security=True;";
+        ="Server=tcp:20.189.122.105,1433; Database=VanessaRestaurant; User Id=sa; Password=Ramos.bsit21;";
+        static SqlConnection sqlConnection = new SqlConnection(connectionString);
+        static public void Connect()
+        {
+            sqlConnection.Open();
+        }
+
+        public List<Menu> GetMenus()
+        {
+            List<Menu> menus = new List<Menu>();
+            string query = "SELECT * FROM MenuList";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    menus.Add(new Menu
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Category = reader["Category"].ToString(),
+                        Item = reader["Item"].ToString(),
+                        Price = reader.GetDecimal(reader.GetOrdinal("Price"))
+                    });
+                }
+            }
+
+            return menus;
+        }
+
+        public Menu GetMenu(string order)
+        {
+            Menu menu = null;
+            string query = "SELECT * FROM MenuList WHERE Item = @Item";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Item", order);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    menu = new Menu
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Category = reader["Category"].ToString(),
+                        Item = reader["Item"].ToString(),
+                        Price = reader.GetDecimal(reader.GetOrdinal("Price"))
+                    };
+                }
+            }
+
+            return menu;
+        }
+    }
+}
